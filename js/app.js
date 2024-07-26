86876deff448f0f8b81211b329305bd1ea6c7629
Edgar Lopez <edgarlopez755@gmail.com>
2024-07-25 18:42:56 -0700

    set project files sceleton notes

---
+++ js/app.js
@@ -0,0 +1,63 @@
+/*-------------------------------- Constants --------------------------------*/
+// 1) 3 user choicew of "rock", "paper", "scissors"
+
+
+
+
+
+
+
+
+
+
+/*-------------------------------- Variables --------------------------------*/
+// 2) Define any variables for state 
+// the players
+// the comp choice
+// the match result - win/lose/draw
+// A result message  - display the result / who won
+
+
+
+
+
+
+
+
+
+
+/*------------------------ Cached Element References ------------------------*/
+// 3) reference to the DOM element to display messages 
+// 3 button DOM references for the rock paper scissors
+
+
+
+
+
+
+
+
+
+
+/*-------------------------------- Functions --------------------------------*/
+// handle player click
+// handle generating random selections for the computer 
+// compare player and computer choices - check for a winner 
+// if/else comparison to all choices 
+// rock beats scissors/ scissors beat paper/ 
+//  render a wi/lose/draw message to the player 
+// incuding player and comp choices in the message 
+// clearly indicating who won/result
+
+
+
+
+
+
+
+
+
+
+
+/*----------------------------- Event Listeners -----------------------------*/
+// handle a player clicking a button 
